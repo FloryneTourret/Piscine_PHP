ec7ba45c2379e201ac445d6d4708efcb913e8d59
Floryne <floryne.tourret.pro@gmail.com>
2019-04-02 16:58:40 +0200

    ex03 ex04

---
+++ d03/ex03/cookie_crisp.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_GET['action']))
+{
+    if ($_GET['action'] == 'set')
+    {    
+        setcookie($_GET['name'], $_GET['value']);
+    }
+    else if ($_GET['action'] == 'get')
+    {
+        if(isset($_COOKIE[$_GET['name']]))
+            echo $_COOKIE[$_GET['name']]."\n";
+    }
+    else if ($_GET['action'] == 'del')
+    {
+        setcookie($_GET['name'], '');
+    }
+}
+
+
+?>
