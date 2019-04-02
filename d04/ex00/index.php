62e7f8a36d0d6543a102c4ad9b357118a83fa5d6
Floryne <floryne.tourret.pro@gmail.com>
2019-04-02 20:28:42 +0200

    debut

--- d04/ex00/index.php
+++ d04/ex00/index.php
@@ -1,20 +1,16 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Session</title>
-</head>
-<body>
+<?php
 
+    session_start();
+    if (isset($_GET['submit']) && $_GET['submit'] =='OK' && isset($_GET['login']) && isset($_GET['passwd']))
+    {
+        $_SESSION['login'] = $_GET['login'];
+        $_SESSION['passwd'] = $_GET['passwd'];
+    }
+?>
+<html><body>
 <form action="index.php" method='get'>
-
-<input type="text" name="login">
-<input type="password" name="passwd">
-<button type="submit" name="submit" value="OK">OK</button>
-
+    <input type="text" name="login" value="<?php if (isset($_SESSION['login'])) echo $_SESSION['login']?>">
+    <input type="password" name="passwd" value="<?php if (isset($_SESSION['passwd'])) echo $_SESSION['passwd']?>">
+    <button type="submit" name="submit" value="OK">OK</button>
 </form>
-    
-</body>
-</html>
+</body></html>
