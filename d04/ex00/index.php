dccfde1dac81154b6510f11fc2ab7ccb58cd4b23
Floryne <floryne.tourret.pro@gmail.com>
2019-04-02 19:58:54 +0200

    debut ex00 d04

---
+++ d04/ex00/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Session</title>
+</head>
+<body>
+
+<form action="index.php" method='get'>
+
+<input type="text" name="login">
+<input type="password" name="passwd">
+<button type="submit" name="submit" value="OK">OK</button>
+
+</form>
+    
+</body>
+</html>
