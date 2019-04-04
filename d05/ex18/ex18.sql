4d76a24b5ab1123795809885d15b2cb12ec9e362
Floryne <floryne.tourret.pro@gmail.com>
2019-04-04 17:51:51 +0200

    update d05

--- d05/ex18/ex18.sql
+++ d05/ex18/ex18.sql
@@ -1,1 +1,1 @@
-SELECT `name` FROM `distrib` WHERE `id_distrib` = 42 OR `id_distrib` = 71 OR `id_distrib` > 61 AND `id_distrib` < 70 OR `id_distrib` > 87 AND `id_distrib` < 91 OR name LIKE '%yy%' OR name LIKE '%y%y%' OR name LIKE '%Y%Y' LIMIT 2, 5;
+SELECT `name` FROM `distrib` WHERE `id_distrib` = 42 OR `id_distrib` = 71 OR (`id_distrib` > 61 AND `id_distrib` < 70) OR (`id_distrib` > 87 AND `id_distrib` < 91) OR `name` LIKE '%yy%' OR `name` LIKE '%y%y%' OR `name` LIKE '%Y%Y' LIMIT 5 OFFSET 2;
