f4caf3ffe533391d0d64e47e2d88c778c493e226
Floryne <floryne.tourret.pro@gmail.com>
2019-04-04 17:20:31 +0200

    d05

---
+++ d05/ex04/ex04.sql
@@ -0,0 +1,1 @@
+UPDATE ft_table SET creation_date = DATE_ADD(creation_date, INTERVAL 20 year) where id > 5;
---
+++ d05/ex17/ex17.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(`id_sub`) AS `nb_susc`, FLOOR(AVG(`price`)) `av_susc`, SUM(`duration_sub`) % 42 AS `ft` FROM `subscription`;
