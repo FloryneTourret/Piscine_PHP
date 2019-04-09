f3c19a5526cdd1223cc998c7afc15fb34bba34c5
Frederic Leonard <fleonard@z4r7p3.le-101.fr>
2019-04-09 13:58:40 +0200

    d06 start

--- d06/ex00/Color.class.php
+++ d06/ex00/Color.class.php
@@ -8,22 +8,54 @@ Class Color{
 
     public static $verbose = FALSE;
 
-    public function __construct( array $rgb){
+    public function __construct(array $rgb){
 
-        if (array_key_exist('rgb', $rgb))
+        if (array_key_exists('rgb', $rgb))
         {
-            $this->red = ($kwargs['rgb'] / (256*256))%256;
-            $this->green = ($kwargs['rgb'] /256) % 256;
-            $this->blue = $kwargs['rgb'] % 256;
+            $this->red = ($rgb['rgb'] / (256*256))%256;
+            $this->green = ($rgb['rgb'] /256) % 256;
+            $this->blue = $rgb['rgb'] % 256;
         }
         else{
-            if(array_key_exist('red', $rgb))
+            if(array_key_exists('red', $rgb))
                 $this->red = $rgb['red'];
-            if(array_key_exist('green', $rgb))
+            if(array_key_exists('green', $rgb))
                 $this->green = $rgb['green'];
-            if(array_key_exist('blue', $rgb))
+            if(array_key_exists('blue', $rgb))
                 $this->blue = $rgb['blue'];
         }
+        if (self::$verbose == True)
+        {
+            printf('Color( red: '.$this->red.', green: '.$this->green.', blue: '.$this->blue.' ) constructed.'."\n");
+        }
+    }
 
+    public function doc()
+    {
+        return (file_get_contents('Color.doc.txt', "r"));
+    }
+
+    public function __toString()
+    {
+        $res = sprintf('Color( red: '.$this->red.', green: '.$this->green.', blue: '.$this->blue.' )');
+        return ($res);
+    }
+    public function add($color)
+    {
+        $colorred = $color->red + $this->red;
+        $colorgreen = $color->green + $this->green;
+        $colorblue = $color->blue + $this->blue;
+
+        $new = new Color(array('red' => $colorred, 'green' => $colorgreen, 'blue' => $colorblue));
+
+        return ($new);
+    }
+
+    public function __destruct()
+    {
+        if (self::$verbose == FALSE)
+        {
+            printf('Color( red: '.$this->red.', green: '.$this->green.', blue: '.$this->blue.' ) destructed.'."\n");
+        }
     }
 }
